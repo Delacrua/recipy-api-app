7fc91316ff874729d19aa25db82e5fa569072085
Uladzimir Fiodarau <ufiodarau@gmail.com>
2022-11-16 15:55:41 +0400

    Implemented first tests

---
+++ src/core/tests/test_models.py
@@ -0,0 +1,27 @@
+import pytest
+
+from django.contrib.auth import get_user_model
+
+
+@pytest.mark.django_db
+def test_create_user_with_email_successful():
+    """Test creating a new user with an email is successfull"""
+    email = 'test@test.com'
+    password = "Test-pass123"
+    user = get_user_model().objects.create_user(
+        email=email,
+        password=password
+    )
+    assert user.email == email
+    assert user.check_password(password)
+
+
+@pytest.mark.django_db
+def test_new_user_email_normalized():
+    """Test the email for new user is normalized"""
+    email = 'test@TEST.COM'
+    user = get_user_model().objects.create_user(
+        email=email,
+        password="Test-pass123"
+    )
+    assert user.email == email.lower()
